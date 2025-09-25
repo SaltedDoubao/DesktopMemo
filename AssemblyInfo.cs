7f0b2ff2183ff92efa4ee85dffc7b70a26c7da3b
SaltedDoubao <salteddoubao@gmail.com>
2025-09-25 17:33:27 +0800

    Prepare to rebuild

--- AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-using System.Windows;
-
-[assembly: ThemeInfo(
-    ResourceDictionaryLocation.None,            // 主题专用资源字典的位置
-                                                //（当某个资源在页面或应用程序资源字典中未找到时使用）
-    ResourceDictionaryLocation.SourceAssembly   // 通用资源字典的位置
-                                                //（当某个资源在页面、应用程序或任何主题专用资源字典中未找到时使用）
-)]
-
